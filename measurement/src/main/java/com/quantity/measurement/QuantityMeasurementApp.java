0931dbadfa6ce5968764ab3423ecffcaeee57d43
simranjeet-kaur-7042 <simranjeet.kaur.4415@gmail.com>
2026-04-01 10:13:41 +0530

    code added to UC3 branch

--- measurement/src/main/java/com/quantity/measurement/QuantityMeasurementApp.java
+++ measurement/src/main/java/com/quantity/measurement/QuantityMeasurementApp.java
@@ -1,37 +1,16 @@
 package com.quantity.measurement;
-
 import com.quantity.measurement.enums.LengthUnit;
 import com.quantity.measurement.model.QuantityLength;
-import java.util.Scanner;
 
 public class QuantityMeasurementApp {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+            double value1 = 1.0;
+            LengthUnit unit1 = LengthUnit.FEET;
 
-        try {
-            System.out.print("Enter first value: ");
-            double value1 = Double.parseDouble(scanner.nextLine());
-            System.out.print("Enter first unit (FEET, INCH): ");
-            String unit1 = scanner.nextLine();
+            double value2 = 12.0;
+            LengthUnit unit2 = LengthUnit.INCH;
 
-            System.out.print("Enter second value: ");
-            double value2 = Double.parseDouble(scanner.nextLine());
-            System.out.print("Enter second unit (FEET, INCH): ");
-            String unit2 = scanner.nextLine();
-
-            // Convert input to uppercase before using valueOf
-            QuantityLength length1 = new QuantityLength(value1, LengthUnit.valueOf(unit1.toUpperCase()));
-            QuantityLength length2 = new QuantityLength(value2, LengthUnit.valueOf(unit2.toUpperCase()));
-
-            if (length1.equals(length2)) {
-                System.out.println("The two lengths are equal.");
-            } else {
-                System.out.println("The two lengths are NOT equal.");
-            }
-
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+            QuantityLength length1 = new QuantityLength(value1, unit1);
+            QuantityLength length2 = new QuantityLength(value2, unit2);  
     }
 }
